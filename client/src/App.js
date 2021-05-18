63916b894709e94bb778d9d2897b861623962737
Nathan Taylor <taylnath@oregonstate.edu>
2021-05-18 16:39:47 -0700

    mern stack part 4 stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
 
 function App() {
   return (
     <div className="App">
       <AppNavbar />
+      <ShoppingList />
     </div>
   );
 }
